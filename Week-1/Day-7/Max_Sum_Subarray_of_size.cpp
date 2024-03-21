38accdd7efb8ffcb785005be6427e8092e9b6609
fpalamin <fpalamin6@gmail.com>
2024-03-21 21:59:58 +0600

    day 7

---
+++ Week-1/Day-7/Max_Sum_Subarray_of_size.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    
+    //  int l = 0, r = 0;
+    //     long long sum = 0, ans = 0;
+    //     while (r < n) {
+    //         sum += a[r];
+    //         if ((r - l + 1) == k) {
+    //             ans = max(ans, sum);
+    //             sum -= a[l];
+    //             l++;
+    //             r++;
+    //         }
+    //         else {
+    //             r++;
+    //         }
+    //     }
+    //     return ans;
+    return 0;
+}
