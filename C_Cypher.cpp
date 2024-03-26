df3f814dbf935d6483b814dead40043eb9e5e155
fpalamin <fpalamin6@gmail.com>
2024-03-26 21:31:21 +0600

    day 5 1 prblem solve

--- C_Cypher.cpp
+++ C_Cypher.cpp
@@ -11,8 +11,26 @@ int main()
     while(t--){
         int n;
         cin>>n;
-        int x,y,z;
-        cin>>x>>y>>z;
+        vector<pair<int,string>> v;
+        
+        while(n--){
+            int x;
+            int c;
+            string s;
+            cin>>x;
+            cin>>c>>s;
+            v.push_back({x,s});
+            // sv.push_back(s);
+        }
+        for(int i=0;i<n;i++){
+            if(v[i].second[i]=='U'){
+                abs(v[i].first--);
+            }
+            if(v[i].second[i]=='D'){
+                abs(v[i].first++);
+            }
+        }
+        for()
     }
    
     return 0;
---
+++ Week-2/Day-5/B_Colourblindness.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string s1, s2;
+        cin >> s1 >> s2;
+        replace(s1.begin(), s1.end(), 'B', 'G');
+        replace(s2.begin(), s2.end(), 'B', 'G');
+        cout << (s1 == s2 ? "YES" : "NO") << '\n';
+    }
+    return 0;
+}
