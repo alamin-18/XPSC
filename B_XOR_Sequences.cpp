4346db4c96e394520ee4937c4bfdef3e512d0e24
fpalamin <fpalamin6@gmail.com>
2024-06-07 20:12:37 +0600

    1 problem sovle

---
+++ B_XOR_Sequences.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // https://codeforces.com/contest/1979/problem/B
+
+    return 0;
+}
