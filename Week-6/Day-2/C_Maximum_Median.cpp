c8249e08d36d17057c4aee934357504276404b4e
fpalamin <fpalamin6@gmail.com>
2024-06-29 18:09:53 +0600

    binary search problem week-6

--- Week-6/Day-2/C_Maximum_Median.cpp
+++ Week-6/Day-2/C_Maximum_Median.cpp
@@ -1,50 +1,45 @@
+/* 
+*_File    :  C_Maximum_Median.cpp",
+            *Writer    :Md Al Amin",
+            *username  : alamin18",
+*_Created :  29 June 2024 ||   17:17:30",
+*/		    
+
 #include <bits/stdc++.h>
 
 using namespace std;
-
 int main()
 {
-
+    
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    
+   
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a.begin(),a.end());
+    auto ok=[&](long long mid){
+        long long cnt=0;
+        for(int i=(n/2);i<n;i++){
+            cnt+=(a[i]<mid ? (mid-a[i]) :0);
 
-    int n, t;
-    cin >> n >> t;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
-
-    auto ok = [&](long long m)
-    {
-        long long total = 0;
-        for (int i = 0; i < n; i++)
-        {
-            total += (m / a[i]);
-            if (total >= t)
-            {
-                return true;
-            }
         }
-        return false;
+        return cnt<=k;
     };
 
-    long long l = 1, r = 1e18, mid, ans;
-    while (l <= r)
-    {
-        mid = l + (r - l) / 2;
-        if (ok(mid))
-        {
-            ans = mid;
-            r = mid - 1;
+    long long l=1,r=2e9,mid,ans=0;
+    while(l<=r){
+        mid =l+(r-l)/2;
+        if(ok(mid)){
+            ans=mid;
+            l=mid+1;
         }
-        else
-        {
-            l = mid + 1;
+        else{
+            r=mid-1;
         }
     }
-
-    cout << ans << '\n';
+    cout<<ans<<'\n';
     return 0;
 }
